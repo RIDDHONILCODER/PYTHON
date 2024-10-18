6b1305f825511226cd471d1259d5c94075ae7cf9
RIDDHONILCODER <riddhonil@gmail.com>
2024-10-18 13:43:45 +0600

    initial commit

---
+++ LESSON1 ACTIVETY 1/main.py
@@ -0,0 +1,14 @@
+name="riddhonil"
+age=10
+school="Aga khan academy"
+hobbies=["football","codding ","gaming"]
+adress="dhaka"
+markes={"english":80,"math":80,"science":80}
+
+print("my name is ",name)
+print("i am ",age)
+print("data type of age is ",type(age))
+print("i love to do ",hobbies)
+print("data type of hobbies is",type(hobbies))
+print("my markes are this ",markes)
+print("data type of markes is",type(markes))
