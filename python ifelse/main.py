af3fbfc2abab712945865d228eb531020cb39fc1
RIDDHONILCODER <riddhonil@gmail.com>
2024-10-23 19:06:19 +0600

    commit

---
+++ python ifelse/main.py
@@ -0,0 +1,17 @@
+print("chouse what do you want to do")
+print("1.play video game")
+print("2.watch dragonball z")
+print("3.study")
+print("4.watch kisena")
+choice=input("enter your choice")
+choice=int(choice)
+if choice==1:
+    print("you want to play video games")
+elif choice==2:
+    print("you want to play dragonball z")
+
+elif choice==3:
+    print("go and study")
+
+elif choice==4:
+    print("watch kisena")
