b367776c026f817f4a556fe060ed490c0fd432d2
RIDDHONILCODER <riddhonil@gmailcom>
2024-12-19 10:19:17 +0600

    initial commit

---
+++ data types on python/main.py
@@ -0,0 +1,10 @@
+a='h'
+print("data type of a is= ",type(a))
+a=True
+print("data type of a is= ",type(a))
+a=1234567834
+print("data type of a is= ",type(a))
+a=0.1
+print("data type of a is= ",type(a))
+
+
--- python print comands/main.py
+++ python print comands/main.py
@@ -1,3 +1,4 @@
+import keyword
 print("welcome to riddhonil's world ")
 print(10)
 print("riddhonil",5)
@@ -10,3 +11,6 @@ favorait_num=10
 print(favorait_num)
 print("the name of my school is ",school)
 print("\n my favorait number is ",favorait_num)
+print("the list of python keywords are ",keyword.kwlist)
+name=input("plese enter your name ?")
+print("nice to meet you ",name)
