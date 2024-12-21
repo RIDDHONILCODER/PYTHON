ed7ced06f51a9af12f0a10a0e606bfa2da4cf8e9
RIDDHONILCODER <riddhonil@gmailcom>
2024-12-21 11:14:08 +0600

    initial commit

---
+++ a 3/main.py
@@ -0,0 +1,12 @@
+#Write a program to check whether the given number is greater than 15 or smaller than 15.
+num=int(input("enter a number"))
+
+if num>15:
+    print(num," greather then 15")
+else:
+    print(num,"less then 15")
+#Write a program to check whether the given number is even or odd?
+if num%2==0:
+    print(num,"the num is even")
+else:
+    print(num,"the num is odd")
--- enter num/main.py
+++ enter num/main.py
@@ -4,3 +4,14 @@ if num>0:
     print(num," is positive.")
 else:
     print(num,"is'nt positive")
+
+#Write to check if a person is buying oranges at 100 rs and later selling it 1at 120 rs. Find that he has profit or loss?
+
+sellingprice=80
+costprice=100
+
+if sellingprice>costprice:
+    profit=sellingprice-costprice
+    print("i am selling at a profit of ",profit)
+else:
+    print("i am selling at a loss")
