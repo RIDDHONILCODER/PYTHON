c81ae3c2dbc1e11bfc0424cb8d1ec9e279936a03
RIDDHONILCODER <riddhonil@gmailcom>
2024-12-24 20:20:14 +0600

    initial commit

---
+++ odd or even/main.py
@@ -0,0 +1,9 @@
+num=int(input("enter a number"))
+if num>15:
+    print(num," greather then 5")
+else:
+    print(num,"less then 5")
+if num%2==0:
+    print(num,"the num is even")
+else:
+    print(num,"the num is odd")
