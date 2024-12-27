ae64d54352ec2a5ee266e60c49d515f51b6d9805
RIDDHONILCODER <riddhonil@gmailcom>
2024-12-27 13:30:34 +0600

    initial commit

---
+++ and or opreator/main.py
@@ -0,0 +1,19 @@
+a=5
+b=10
+c=0
+if a and b and c:
+    print("all values are boolean")
+else:
+    print("either one of them are not boolean")
+if a>b and a>c:
+    print("the greatest number is ",a)
+elif b>a and b>c:
+    print("the greatest number is ",b)
+else:
+    print("the greatest number is ",c)
+if a!=b:
+    print("a and b are not same")
+name="riddhonil"
+title="ghosh"
+if name!=title:
+    print("their diffrent")
