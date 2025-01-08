ac00dbbcdd3e062b2eeb0822b37beeb19dd46809
RIDDHONILCODER <riddhonil@gmailcom>
2025-01-08 19:12:08 +0600

    initial commit

---
+++ revers odder/main.py
@@ -0,0 +1,6 @@
+#Write a program to print the numbers in reverse order beginning from the number entered by the user.
+
+num=int(input("enter a number from where you want to start the revers"))
+
+for i in range(num,0,-1):
+    print(i)
