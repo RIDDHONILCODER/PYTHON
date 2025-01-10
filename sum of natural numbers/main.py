68d04442477b9b07eb3261593d4e2ee4763003ba
RIDDHONILCODER <riddhonil@gmailcom>
2025-01-10 13:20:56 +0600

    initial commit

---
+++ sum of natural numbers/main.py
@@ -0,0 +1,12 @@
+#Write a program to find the sum of natural numbers.
+
+n=int(input("Enter the number"))
+
+i=1
+
+sum=0
+
+while i<=n:
+    sum=sum+i
+    i=i+1
+print("the sum of natural numbers are  ",sum )
