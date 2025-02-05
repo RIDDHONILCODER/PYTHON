f33a09d47895fd910efb4883691225312c16049c
RIDDHONILCODER <riddhonil@gmailcom>
2025-02-05 18:23:09 +0600

    initial commit

---
+++ check alphabets/main.py
@@ -0,0 +1,6 @@
+character=input("enter a character")
+for i in characters:
+    if character==abcdefghijklmnopqrstuvwxy:
+        print("thanks")
+    else:
+        print("check what have you entered")    
