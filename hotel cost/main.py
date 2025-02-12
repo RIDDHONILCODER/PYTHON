c87e070857ee5c65df6c3fe930f4a8f80cdba9c0
RIDDHONILCODER <riddhonil@gmailcom>
2025-02-12 19:18:33 +0600

    initial commit

---
+++ hotel cost/main.py
@@ -0,0 +1,9 @@
+def hotelrent(days):
+    print("the hotel rent is ",144*days)
+def carcost(days):
+    print("the car rent is ",70*days)
+days=int(input("how many days is your trip"))
+
+hotelrent(days)
+
+carcost(days)
