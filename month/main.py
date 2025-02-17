b1ac2fee275076f7fa77b6531acd5b63c8fa0978
RIDDHONILCODER <riddhonil@gmailcom>
2025-02-17 18:15:44 +0600

    initial commit

---
+++ month/main.py
@@ -0,0 +1,19 @@
+import calendar
+import time
+
+year=2025
+month=1
+print("the month of the year () is ",calendar)
+
+year=2025
+month=2
+print("the month of the year () is ",calendar)
+
+year=2025
+month=3
+print("the month of the year () is ",calendar)
+
+year=2025
+month=4
+print("the month of the year () is ",calendar)
+
