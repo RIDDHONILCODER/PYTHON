2faa9f7da79c48866fb45d738cfbbee6c9433357
RIDDHONILCODER <riddhonil@gmailcom>
2025-02-21 17:45:17 +0600

    initial commit

--- dictionary 2/main.py
+++ dictionary 2/main.py
@@ -5,4 +5,7 @@ for key,value in dict.items():
     if value==2:
         count=count+1
 print("the frequency of the value 2 is ",count)
-    
+#Write a program to return the country code for various countries. Here’s a dictionary of different country codes - {'India' : '0091', 'Australia' : '0025', 'Nepal' : '00977'}.
+country_dict={'India' : '0091', 'Australia' : '0025', 'Nepal' : '00977'}
+print("the code for country nepal is ",country_dict['Nepal'])
+print("the code for country bangladesh is ",country_dict.get('bangladesh','not found'))
