131d9dacdeb1f1f4b01c83f789a1f8adb72e4297
RIDDHONILCODER <riddhonil@gmailcom>
2025-03-12 18:49:32 +0600

    initial commit

---
+++ oop/main.py
@@ -0,0 +1,5 @@
+#Write a program to create a class with the name Student and perform the following tasks - 1. Declare a variable grade 2. Print a sentence inside the class 3. Create an object of class student and see the output
+class student:
+    grade=5
+    print("riddhonils grade is ",grade)
+st=student()
