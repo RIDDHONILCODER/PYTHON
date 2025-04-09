a23eb143ac677a940bc50051df691bb7e9703b85
RIDDHONILCODER <riddhonil@gmailcom>
2025-04-09 19:23:27 +0600

    initial commit

--- pygame with rectangle/main.py
+++ pygame with rectangle/main.py
@@ -5,10 +5,29 @@ pygame.init()
 screen=pygame.display.set_mode((300,400))
 
 done=False
+x=20
+y=20
+width=100
+height=200
+
+clock=pygame.time.Clock()  
 
 while not done:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             done=True
-    pygame.draw.rect(screen,(0,100,200),pygame.Rect(20,20,100,200))
+    pressed=pygame.key.get_pressed()
+    if pressed[pygame.K_LEFT]:
+        x -=3
+    if pressed[pygame.K_RIGHT]:
+        x +=3
+    if pressed[pygame.K_UP]:
+        y -=3    
+    if pressed[pygame.K_DOWN]:
+        y +=3    
+    screen.fill((0,0,0))
+    pygame.draw.rect(screen,(0,100,200),pygame.Rect(x,y,width,height))
     pygame.display.flip()
+    clock.tick(90)
+
+    
