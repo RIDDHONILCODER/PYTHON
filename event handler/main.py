d7596b7cafc3593e5c15c0605ec114070ba37ce9
RIDDHONILCODER <riddhonil@gmailcom>
2025-05-07 19:14:39 +0600

    initial commit

--- event handler/main.py
+++ event handler/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 window=Tk()
 window.title("EVENT HANDLER")
 window.geometry("100x100")
@@ -8,8 +9,16 @@ def handler_click(event):
 
 def key_presed(event):
     print("the key presed is ",event.char)
+
+def msg():
+    messagebox.showwarning("scaning for virus")
+    
+
 button=Button(text="click me")
 button.pack()
+
+button2=Button(text="scan for virus", command=msg)
+button2.pack()
 
 button.bind("<Button-1>",handler_click)
 window.bind("<Key>",key_presed)
